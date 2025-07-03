9ceb94c8938a3a04ee2a0c233a2f4370981a6c36
The HTML Project <159310111+TheHTMLProject@users.noreply.github.com>
2025-07-03 16:38:26 -0700

    Update script.js

--- script/landers/preview/script.js
+++ script/landers/preview/script.js
@@ -1,5 +1,5 @@
            document.addEventListener("DOMContentLoaded", function () {
-    const currentMessage = "This website is just an embedded preview of the real thing. Fullscreening games will not work. If you would like to see my website at its full functionality, please visit https://tinyurl.com/thehtmlsl/"; 
+    const currentMessage = "This website is just an embedded preview of the real thing. Fullscreening games will not work. If you would like to see my website at its full functionality, please visit https://thehtmlproject.com"; 
 
     if (localStorage.getItem("popupMessage") !== currentMessage) {
         alert(currentMessage);
