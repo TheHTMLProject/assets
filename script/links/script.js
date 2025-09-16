504857b3becb675d4de6d3e07658531b89aab23f
Greenr <159310111+GreenrCode@users.noreply.github.com>
2025-09-16 12:02:52 -0700

    Update script.js

--- script/links/script.js
+++ script/links/script.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
     function applyDarkMode() {
         body.classList.add("dark");
         body.classList.remove("light");
-        siteLogo.src = "https://assets.thehtmlproject.com/img/logodark.png";
+        siteLogo.src = "https://assets.greenrcode.com/img/logodark.png";
     }
 
     function applyLightMode() {
         body.classList.remove("dark");
         body.classList.add("light");
-        siteLogo.src = "https://assets.thehtmlproject.com/icon.jpeg";
+        siteLogo.src = "https://assets.greenrcode.com/icon.jpeg";
     }
 
 
