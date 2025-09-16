85831ad324a753ff480e91680840c6a9beb2ea62
Greenr <159310111+GreenrCode@users.noreply.github.com>
2025-09-16 12:02:14 -0700

    Update script.js

--- script/landers/preview/script.js
+++ script/landers/preview/script.js
@@ -1,5 +1,5 @@
            document.addEventListener("DOMContentLoaded", function () {
-    const currentMessage = "This website is just an embedded preview of the real thing. Fullscreening games will not work. If you would like to see my website at its full functionality, please visit https://thehtmlproject.com"; 
+    const currentMessage = "This website is just an embedded preview of the real thing. Fullscreening games will not work. If you would like to see my website at its full functionality, please visit https://greenrcode.com"; 
 
     if (localStorage.getItem("popupMessage") !== currentMessage) {
         alert(currentMessage);
